cce4f7dcf6b8289bced17d54e13392e5550b32a8
bswierzewski <swierzewski.bartosz@gmail.com>
2025-08-31 06:37:56 +0200

    Fix using namespaces

--- backend/Host/Autodor.Web/Program.cs
+++ backend/Host/Autodor.Web/Program.cs
@@ -1,6 +1,7 @@
 using Autodor.Modules.Contractors.Infrastructure;
 using Autodor.Modules.Invoicing.Infrastructure;
 using Autodor.Modules.Orders.Infrastructure;
+using Autodor.Modules.Products.Infrastructure;
 using Autodor.Web.Endpoints;
 using Autodor.Web.Services;
 using SharedKernel.Application;
