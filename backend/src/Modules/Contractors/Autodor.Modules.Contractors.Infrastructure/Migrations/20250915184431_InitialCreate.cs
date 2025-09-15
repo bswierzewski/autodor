40e6aa75e14f145b50b930273bc57bc00913b7c7
bswierzewski <swierzewski.bartosz@gmail.com>
2025-09-15 21:17:31 +0200

    MIgration fix

---
+++ backend/src/Modules/Contractors/Autodor.Modules.Contractors.Infrastructure/Migrations/20250915184431_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Autodor.Modules.Contractors.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contractors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NIP = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Email = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Address_City = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Address_Street = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Address_ZipCode = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contractors", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contractors_NIP",
+                table: "Contractors",
+                column: "NIP",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contractors");
+        }
+    }
+}
