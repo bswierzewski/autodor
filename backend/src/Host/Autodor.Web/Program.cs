c33450eecdcdb12b50317fd170e02457338cb59e
bswierzewski <swierzewski.bartosz@gmail.com>
2025-10-26 07:48:56 +0100

    Update packages

--- backend/src/Host/Autodor.Web/Program.cs
+++ backend/src/Host/Autodor.Web/Program.cs
@@ -32,15 +32,14 @@ builder.Services.AddOrders(builder.Configuration);
 builder.Services.AddInvoicing();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseAuthentication();
