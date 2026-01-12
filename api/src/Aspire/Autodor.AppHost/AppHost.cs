326ce43c30cf5422b3baafb15bc66607e9cfa4f7
bswierzewski <swierzewski.bartosz@gmail.com>
2026-01-12 22:12:47 +0100

    Add Api

--- api/src/Aspire/Autodor.AppHost/AppHost.cs
+++ api/src/Aspire/Autodor.AppHost/AppHost.cs
@@ -1,4 +1,6 @@
 // Initializes and runs the Aspire orchestration host that manages all microservices
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Autodor_Api>("api");
+
 builder.Build().Run();
